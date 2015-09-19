f577e8af22aff371f8f6b7294b513d10ae64e744
NNaka <norihito.naka@tufts.edu>
2015-09-19 16:37:01 -0400

    Fixed URL bug

--- travel.js
+++ travel.js
@@ -1,7 +1,32 @@
 function startTravelSearch() {
-  console.log("In start travel search");
+  var origin = "IST";
+  var destination = "BOS";
+  var departure_date = "2015-10-15";
+  var return_date = "2015-10-21";
+  var apikey = "bYIxdGBOq7VRuiWIGsvwL12PDMV9TZmU";
+  var url = "http://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?origin=%s&"
+    + "destination=" + destination + "&"
+    + "departure_date=" + departure_date + "&"
+    + "return_date=" + return_date + "&"
+    + "apikey=" + apikey;
+
+  console.log(url);
+
   var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+  xmlHttp.open( "GET", url, false ); // false for synchronous request
   xmlHttp.send( null );
   return xmlHttp.responseText;
 }
+
+$(document).on('click', '#subscribeBtn', function() {
+  $.ajax({
+
+    url: '/users/subscribe',
+    type: 'GET',
+    success: function () {
+      console.log('subscribed');
+      $("#accountUnsubscribed").hide();
+      $("#accountSubscribed").show();
+    }
+  });
+});
