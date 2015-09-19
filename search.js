4aa4ef03ff61b5c232ab75180142296d0874e467
Dirk <stahdirk@mit.edu>
2015-09-19 16:20:28 -0400

    added many fields; jquery; beginning search support

--- search.js
+++ search.js
@@ -1,11 +1,26 @@
-var input = "http://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?origin=IST&destination=BOS&departure_date=2015-10-15&return_by=2015-10-21T20:00&adults=2&children=3&infants=1&direct=true&include_airlines=TK&apikey=<your API key>"
-
-function httpGet(theUrl)
-{
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-  xmlHttp.send( null );
-  return xmlHttp.responseText;
+function addImage(path) {
+    var html = "<img src=\"" + path + "\" />"
+    $("#imageArea").html(html);
 }
 
+function startImageSearch() {
+    var searchText = $('#searchText').val();
 
+    var tags = localstorage.searchText;
+
+    if (tags == "") { //empty, not in database
+
+    }
+    else {
+        var imageInfoObj = JSON.parse(tags);
+        for (var i = 0; i < imageInfoObj; i++) {
+
+        }
+    }
+}
+
+function addObjToDatabase() {
+    var key = $('#addKey').val();
+    var value = $('#addValue').val();
+    localstorage.key = value;
+}
